3beb88560793727c5a85ed24e0d8759b5449684e
Daniel Lima <pdaniel643@gmail.com>
2023-12-31 13:52:09 -0300

    (Part 2) Log, LogFileMixin, LogPrintMixin and the union of everything so far
    Signed-off-by: Daniel Lima <pdaniel643@gmail.com>

--- advanced_module/Object_Oriented_Programming/abstraction/log_test/log.py
+++ advanced_module/Object_Oriented_Programming/abstraction/log_test/log.py
@@ -1,14 +1,23 @@
 # Abstraction
 class Log:
-    def log(self, msg):
+    def _log(self, msg):
         raise NotImplementedError('Implement the log method')
 
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
 
 class LogFileMixin(Log):
-    def log(self, msg):
+    def _log(self, msg):
         print(msg)
-
+        
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
 
 if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log('anything')
+    l = LogPrintMixin()
+    l.log_error('anything')
+    l.log_success('Cool')
