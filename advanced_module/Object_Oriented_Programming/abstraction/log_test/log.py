2c6721b05b3e13f26825eab4034dec42bd2616ec
Daniel Lima <pdaniel643@gmail.com>
2023-12-31 13:41:22 -0300

    (Part 1) Mixins, Abstraction and the union of everything so far
    Signed-off-by: Daniel Lima <pdaniel643@gmail.com>

---
+++ advanced_module/Object_Oriented_Programming/abstraction/log_test/log.py
@@ -0,0 +1,14 @@
+# Abstraction
+class Log:
+    def log(self, msg):
+        raise NotImplementedError('Implement the log method')
+
+
+class LogFileMixin(Log):
+    def log(self, msg):
+        print(msg)
+
+
+if __name__ == '__main__':
+    l = LogFileMixin()
+    l.log('anything')
